acf65f51dc27650a52cfdd4255c9295fc8255720
Timothy Yu <tmichael.yu@gmail.com>
2019-02-01 16:31:04 -0500

    git subrepo clone (merge) https://github.com/jonromero/forex_algotrading.git
    subrepo:
      subdir:   "forex_algotrading"
      merged:   "220f00c"
    upstream:
      origin:   "https://github.com/jonromero/forex_algotrading.git"
      branch:   "master"
      commit:   "220f00c"
    git-subrepo:
      version:  "0.4.0"
      origin:   "???"
      commit:   "???"

---
+++ forex_algotrading/post3/runner.py
@@ -0,0 +1,47 @@
+"""
+The main file that will evolve into our trading library
+"""
+from datetime import datetime, timedelta
+import v20
+
+
+OANDA_ACCESS_TOKEN = ""
+OANDA_ACCOUNT_ID = '' #put your access id here 
+
+def main():
+    print("------ System online -------", datetime.now())
+    latest_price_time = (datetime.utcnow() - timedelta(seconds=15)).isoformat('T')+'Z'
+
+    api = v20.Context(
+            'api-fxpractice.oanda.com',
+            '443',
+            token=OANDA_ACCESS_TOKEN)
+
+    response = api.pricing.get(
+                    OANDA_ACCOUNT_ID,
+                    instruments='EUR_USD',
+                    since=latest_price_time,
+                    includeUnitsAvailable=False)
+
+    print(response.reason + latest_price_time)
+    
+    prices = response.get("prices", 200)
+    if len(prices):
+        buy_price = prices[0].bids[0].price 
+
+        print("Buy at " + str(buy_price))
+
+        response = api.order.market(
+            OANDA_ACCOUNT_ID,
+            instrument='EUR_USD',
+            units=5000)
+
+        print("Trading id: " + str(response.get('orderFillTransaction').id))
+        print("Account Balance: " + str(response.get('orderFillTransaction').accountBalance))
+        print("Price: " + str(response.get('orderFillTransaction').price))
+        
+    else:
+        print(response.reason)
+
+if __name__ == "__main__":
+    main()
