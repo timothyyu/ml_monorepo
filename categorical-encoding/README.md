ffd1f175767e96605550bb264fc1904b7ab2dae3
Timothy Yu <tmichael.yu@gmail.com>
2019-02-01 16:51:50 -0500

    git subrepo clone https://github.com/scikit-learn-contrib/categorical-encoding.git
    subrepo:
      subdir:   "categorical-encoding"
      merged:   "ee691c6c"
    upstream:
      origin:   "https://github.com/scikit-learn-contrib/categorical-encoding.git"
      branch:   "master"
      commit:   "ee691c6c"
    git-subrepo:
      version:  "0.4.0"
      origin:   "???"
      commit:   "???"

---
+++ categorical-encoding/README.md
@@ -0,0 +1,123 @@
+Categorical Encoding Methods
+============================
+
+[![Travis Status](https://travis-ci.org/scikit-learn-contrib/categorical-encoding.svg?branch=master)](https://travis-ci.org/scikit-learn-contrib/categorical-encoding)
+[![Coveralls Status](https://coveralls.io/repos/scikit-learn-contrib/categorical-encoding/badge.svg?branch=master&service=github)](https://coveralls.io/r/scikit-learn-contrib/categorical-encoding)
+[![CircleCI Status](https://circleci.com/gh/scikit-learn-contrib/categorical-encoding.svg?style=shield&circle-token=:circle-token)](https://circleci.com/gh/scikit-learn-contrib/categorical-encoding/tree/master)
+[![DOI](https://zenodo.org/badge/47077067.svg)](https://zenodo.org/badge/latestdoi/47077067)
+
+A set of scikit-learn-style transformers for encoding categorical 
+variables into numeric by means of different techniques.
+
+Important Links
+---------------
+
+Documentation: [http://contrib.scikit-learn.org/categorical-encoding/](http://contrib.scikit-learn.org/categorical-encoding/)
+
+Encoding Methods
+----------------
+
+ * Backward Difference Contrast [2][3]
+ * BaseN [6]
+ * Binary [5]
+ * Hashing [1]
+ * Helmert Contrast [2][3]
+ * LeaveOneOut [4]
+ * Ordinal [2][3]
+ * One-Hot [2][3]
+ * Polynomial Contrast [2][3]
+ * Sum Contrast [2][3]
+ * Target Encoding [7]
+ * Weight of Evidence [8]
+
+Usage
+-----
+
+The package by itself comes with a single module and an estimator. Before
+installing the module you will need `numpy`, `statsmodels`, and `scipy`.
+
+To install the module execute:
+
+```shell
+$ python setup.py install
+```
+
+or 
+
+```
+pip install category_encoders
+```
+
+or
+
+```
+conda install -c conda-forge category_encoders
+```
+    
+To use:
+
+    import category_encoders as ce
+    
+    encoder = ce.BackwardDifferenceEncoder(cols=[...])
+    encoder = ce.BaseNEncoder(cols=[...])
+    encoder = ce.BinaryEncoder(cols=[...])
+    encoder = ce.HashingEncoder(cols=[...])
+    encoder = ce.HelmertEncoder(cols=[...])
+    encoder = ce.LeaveOneOutEncoder(cols=[...])
+    encoder = ce.OneHotEncoder(cols=[...])
+    encoder = ce.OrdinalEncoder(cols=[...])
+    encoder = ce.PolynomialEncoder(cols=[...])
+    encoder = ce.SumEncoder(cols=[...])
+    encoder = ce.TargetEncoder(cols=[...])
+    encoder = ce.WOEEncoder(cols=[...])
+
+All of these are fully compatible sklearn transformers, so they can be used in pipelines or in your existing scripts. Supported input formats include numpy arrays and pandas dataframes. If the cols parameter isn't passed, all columns with object or pandas categorical data type will be encoded. Please see the docs for transformer-specific configuration options.
+
+Examples
+--------
+
+    from category_encoders import *
+    import pandas as pd
+    from sklearn.datasets import load_boston
+
+    # prepare some data
+    bunch = load_boston()
+    y = bunch.target
+    X = pd.DataFrame(bunch.data, columns=bunch.feature_names)
+
+    # use binary encoding to encode two categorical features
+    enc = BinaryEncoder(cols=['CHAS', 'RAD']).fit(X, y)
+
+    # transform the dataset
+    numeric_dataset = enc.transform(X)
+
+In the examples directory, there is an example script used to benchmark
+different encoding techniques on various datasets.
+
+The datasets used in the examples are car, mushroom, and splice datasets 
+from the UCI dataset repository, found here:
+
+[datasets](https://archive.ics.uci.edu/ml/datasets)
+
+Contributing
+------------
+
+Category encoders is under active development, if you'd like to be involved, we'd love to have you. Check out the CONTRIBUTING.md file
+or open an issue on the github project to get started.
+
+License
+-------
+
+BSD 3-Clause
+
+References:
+-----------
+
+ 1. Kilian Weinberger; Anirban Dasgupta; John Langford; Alex Smola; Josh Attenberg (2009). Feature Hashing for Large Scale Multitask Learning. Proc. ICML.
+ 2. Contrast Coding Systems for categorical variables.  UCLA: Statistical Consulting Group. from https://stats.idre.ucla.edu/r/library/r-library-contrast-coding-systems-for-categorical-variables/.
+ 3. Gregory Carey (2003). Coding Categorical Variables. from http://psych.colorado.edu/~carey/Courses/PSYC5741/handouts/Coding%20Categorical%20Variables%202006-03-03.pdf
+ 4. Strategies to encode categorical variables with many categories. from https://www.kaggle.com/c/caterpillar-tube-pricing/discussion/15748#143154.
+ 5. Beyond One-Hot: an exploration of categorical variables. from http://www.willmcginnis.com/2015/11/29/beyond-one-hot-an-exploration-of-categorical-variables/
+ 6. BaseN Encoding and Grid Search in categorical variables. from http://www.willmcginnis.com/2016/12/18/basen-encoding-grid-search-category_encoders/
+ 7. Daniele Miccii-Barreca (2001). A Preprocessing Scheme for High-Cardinality Categorical Attributes in Classification and Prediction Problems. SIGKDD Explor. Newsl. 3, 1. from http://dx.doi.org/10.1145/507533.507538
+ 8. Weight of Evidence (WOE) and Information Value Explained. from https://www.listendata.com/2015/03/weight-of-evidence-woe-and-information.html
---
+++ categorical-encoding/examples/benchmarking_large/benchmarking_large.py
@@ -0,0 +1,122 @@
+"""
+Run a large scale benchmark.
+
+We measure: {dataset, encoder, model, train and test accuracy measures, train and test runtimes, feature count}.
+
+Note: A reasonably recent version of sklearn is required to run GradientBoostingClassifier and MLPClassifier.
+"""
+import os
+import warnings
+
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+import category_encoders
+from examples.benchmarking_large import arff_loader
+from examples.benchmarking_large.util import train_model, train_encoder
+
+# The settings are taken from:
+#   Data-driven advice for applying machine learning to bioinformatics problems, Olson et al.
+# Following models have high variance of results: SGD, SVC and DecisionTree. That is not a big deal.
+# Just be careful during the result interpretation.
+# Also, following models are slow because of their configuration: GradientBoosting and RandomForest.
+# SGD and DecisionTree benefit from stronger regularization.
+models = [SGDClassifier(loss='modified_huber', max_iter=50, tol=1e-3, random_state=2001),
+          LogisticRegression(C=1.5, penalty='l1', fit_intercept=True, solver='liblinear'),  # ElasticNet would rid us of overflows in the model
+          SVC(kernel='poly', probability=True, C=0.01, gamma=0.1, degree=3, coef0=10.0, random_state=2001),
+          KNeighborsClassifier(),
+          GaussianNB(),
+          DecisionTreeClassifier(max_depth=4, random_state=2001),
+          GradientBoostingClassifier(loss='deviance', learning_rate=0.1, n_estimators=500, max_depth=3, max_features='log2', random_state=2001),
+          RandomForestClassifier(n_estimators=500, max_features=0.25, criterion='entropy', random_state=2001),
+          MLPClassifier(max_iter=200, n_iter_no_change=2, tol=1e-3, random_state=2001)]
+
+# We use Arff datasets on GitHub. But once OpenML loader will be part of scikit-learn:
+#   https://github.com/scikit-learn/scikit-learn/pull/11419
+# the plan is to move on OpenML.
+# We ignore datasets without any polynomial feature.
+# We also ignore 'splice.arff', 'anneal.arff', 'anneal.orig.arff' due to high runtime.
+# Datasets sensitive to amount of regularization are:
+#     breast.cancer.arff                Medium impact
+#     bridges.version1.arff                             (contains an ID)
+#     bridges.version2.arff                             (contains an ID)
+#     car.arff
+#     colic.arff
+#     cylinder.bands.arff               Medium impact   (contains a constant column + almost an ID)
+#     flags.arff                        Large impact
+#     heart.c.arff
+#     hepatitis.arff
+#     hypothyroid.arff                                  (contains a constant column)
+#     kr.vs.kp.arff
+#     labor.arff                        Large impact
+#     lymph.arff
+#     nursery.arff
+#     postoperative.patient.data.arff   Large impact
+#     primary.tumor.arff
+#     solar.flare1.arff                 Medium impact
+#     solar.flare2.arff                 Medium impact
+#     soybean.arff                      Large impact
+#     sick.arff
+#     spectrometer.arff                 Large impact    (contains an ID)
+#     sponge.arff                       Large impact
+#     tic-tac-toe.arff
+#     trains.arff                       Medium impact   (tiny dataset -> with high variance)
+datasets = ['audiology.arff', 'autos.arff', 'breast.cancer.arff', 'bridges.version1.arff', 'bridges.version2.arff', 'car.arff',
+            'colic.arff', 'credit.a.arff', 'credit.g.arff', 'cylinder.bands.arff', 'flags.arff', 'heart.c.arff', 'heart.h.arff',
+            'hepatitis.arff', 'hypothyroid.arff', 'kr.vs.kp.arff', 'labor.arff', 'lymph.arff', 'mushroom.arff', 'nursery.arff',
+            'postoperative.patient.data.arff', 'primary.tumor.arff', 'sick.arff', 'solar.flare1.arff', 'solar.flare2.arff',
+            'soybean.arff', 'spectrometer.arff', 'sponge.arff', 'tic-tac-toe.arff', 'trains.arff', 'vote.arff', 'vowel.arff']
+
+# We painstakingly initialize each encoder here because that gives us the freedom to initialize the
+# encoders with any setting we want.
+encoders = [ #category_encoders.BackwardDifferenceEncoder(),
+             category_encoders.BaseNEncoder(),
+             category_encoders.BinaryEncoder(),
+             category_encoders.HashingEncoder(),
+             # category_encoders.HelmertEncoder(),
+             category_encoders.JamesSteinEncoder(),
+             category_encoders.LeaveOneOutEncoder(),
+             category_encoders.MEstimateEncoder(),
+             category_encoders.OneHotEncoder(),
+             category_encoders.OrdinalEncoder(),
+             # category_encoders.PolynomialEncoder(),
+             # category_encoders.SumEncoder(),
+             category_encoders.TargetEncoder(),
+             category_encoders.WOEEncoder()]
+
+# Initialization
+if os.path.isfile('./output/result.csv'):
+    os.remove('./output/result.csv')
+
+# Loop over datasets, then over encoders, and finally, over the models
+for dataset_name in datasets:
+    X, y, fold_count = arff_loader.load(dataset_name)
+    non_numeric = list(X.select_dtypes(exclude=[np.number]).columns.values)
+    for encoder in encoders:
+        print("Encoding:", dataset_name, y.name, encoder.__class__.__name__)
+        folds, fit_encoder_time, score_encoder_time = train_encoder(X, y, fold_count, encoder)
+        for model in models:
+            print('Evaluating:', dataset_name, encoder.__class__.__name__, model.__class__.__name__)
+            scores, fit_model_time, score_model_time = train_model(folds, model)
+
+            # Log into csv
+            result = pd.DataFrame([dataset_name, y.name, encoder.__class__.__name__, model.__class__.__name__, X.shape[1],
+                                   folds[0][0].shape[1], fit_encoder_time, score_encoder_time, fit_model_time, score_model_time]
+                                  + list(scores)).T
+            if not os.path.isfile('./output/result.csv'):
+                result.to_csv('./output/result.csv',
+                              header=['dataset', 'target', 'encoder', 'model', 'input_features', 'output_features', 'fit_encoder_time',
+                                      'score_encoder_time', 'fit_model_time', 'score_model_time', 'test_matthews', 'train_matthews',
+                                      'test_auc', 'train_auc', 'test_brier', 'train_brier'], index=False)
+            else:
+                result.to_csv('./output/result.csv', mode='a', header=False, index=False)
+
+print('Finished. The result was stored into ./output/result.csv.')
